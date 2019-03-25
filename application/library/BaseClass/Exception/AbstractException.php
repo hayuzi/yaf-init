d930c567e7a9d6a12581518c730c7c27c937bc1f
hayuzi <hayuzi@163.com>
2019-03-25 22:46:53 +0800

    use medoo

--- application/library/BaseClass/Exception/AbstractException.php
+++ application/library/BaseClass/Exception/AbstractException.php
@@ -13,7 +13,7 @@ use Yaf\Exception;
 class AbstractException extends Exception
 {
 
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    public function __construct($message = "", $code = 0, \Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }

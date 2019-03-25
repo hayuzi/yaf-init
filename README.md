bc08832a63a0d13aff7d167f46c6980db8603a87
hayuzi <hayuzi@163.com>
2019-03-25 22:54:29 +0800

    use medoo

---
+++ README.md
@@ -0,0 +1,11 @@
+yaf-init
+==
+
+> yaf的初始化框架，便于调用
+
+### 1. 封装扩展的内容
+#### 1.1 封装了基础 model 类
+Model类采用 [Medoo](https://github.com/catfan/Medoo), 该类是基于PDO的封装, 但是非ORM形式，因而处理关联数据可能稍微有些不便。
+
+#### 1.2 路由与请求
+
---
+++ application/library/BaseClass/RequestBase.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hayuzi
+ * Date: 2019/3/25
+ * Time: 下午10:48
+ */
+
+namespace BaseClass;
+
+
+class RequestBase
+{
+    /**
+     * TODO request 请求待封装， 并且需要封装验证器
+     */
+
+}
